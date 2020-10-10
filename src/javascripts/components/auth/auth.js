da0aa9822e42d160a8077f8bfd8a28a88bd0da5c
Chris LoJacono <clojacono92@gmail.com>
2020-10-10 12:49:14 -0500

    login button

--- src/javascripts/components/auth/auth.js
+++ src/javascripts/components/auth/auth.js
@@ -5,3 +5,18 @@ const signMeIn = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().signInWithPopup(provider);
 };
+
+const loginButton = () => {
+    const domString = `<div id="auth">
+                        <span style="font-size: 5em; color: Tomato;"><i class="fas fa-utensils"></i></span>
+                        <h4>Login to Le Baquette</h4>
+                        <button id="google-auth" class="btn btn-primary btn-lg">
+                          <i class="fab fa-google"></i></i>oogle Login
+                        </button>
+                      </div>`;
+  
+    $('#app').html(domString);
+    $('#google-auth').on('click', signMeIn);
+};
+  
+  export default { loginButton };
