2014713b8ed429c6109a138203866a17bbd56578
josephtmartin <goldglovejo13@gmail.com>
2020-10-10 13:21:12 -0500

    added the html for the navbar

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,8 @@
 import '../styles/main.scss';
+import navbar from './components/navbar/navbar';
 
 const init = () => {
-  $('#app').html('<h1>HELLO! You are up and running!</h1>');
-  console.log('YOU ARE UP AND RUNNING!');
+  navbar.navbar();
 };
 
 init();
