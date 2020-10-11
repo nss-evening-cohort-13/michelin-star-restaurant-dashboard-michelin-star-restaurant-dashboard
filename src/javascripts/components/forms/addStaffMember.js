d0261c8777316c77777aeeb78a10107c3f37f61f
Liz <liz@Liz-s-Mac.local>
2020-10-11 11:53:13 -0500

    console form values on 'add staff member' btn click

--- src/javascripts/components/forms/addStaffMember.js
+++ src/javascripts/components/forms/addStaffMember.js
@@ -5,20 +5,29 @@ const addStaffMemberForm = () => {
   <div id="error-message"></div>
   <div class="form-group row">
       <label for="name">Name</label>
-      <input type="text" class="form-control" id="name" placeholder="Example: Stefan">
+      <input type="text" class="form-control" id="name" placeholder="Example: Gordon Ramsay ">
   </div>
   <div class="form-group row">
       <label for="role">Role</label>
-      <input type="text" class="form-control" id="name" placeholder="Example: Chef">
+      <input type="text" class="form-control" id="role" placeholder="Example: Chef">
   </div>
   <div class="form-group row">
       <label for="imageUrl">Image</label>
-      <input type="text" class="form-control" id="" placeholder="Place Image URL">
+      <input type="text" class="form-control" id="staff-image-url" placeholder="Place Image URL">
   </div>
     <button id="add-staff-btn" type="submit" class="btn btn-info form-btn"><i class="fas fa-plus-circle"></i> Add Staff Member</button>
     `);
+
   $('#add-staff-btn').on('click', () => {
     console.warn('clicked add btn');
+
+    const staffData = {
+      name: $('#name').val() || false,
+      role: $('#role').val() || false,
+      imageUrl: $('#staff-image-url').val() || false,
+    };
+
+    console.warn(staffData, 'staff data');
   });
 };
 
