828aef00064e5a851acee2b94389ea814a2aa3c3
Liz <liz@Liz-s-Mac.local>
2020-10-11 16:48:00 -0500

    styled card buttons to have same color scheme

--- src/javascripts/components/cards/staffMemberCard.js
+++ src/javascripts/components/cards/staffMemberCard.js
@@ -3,8 +3,8 @@ const buildStaffCard = (staffObject) => {
           <div class="staff-container card-container" id="${staffObject.firebaseKey}">
             <div class="staff-card card" id="${staffObject.firebaseKey}" style="width: 100%; background-image: url(${staffObject.imageUrl});">
               <div class="staff-btn-container card-btn-container">
-                <button class="btn btn-danger edit-staff-btn edit-btn" id="${staffObject.firebaseKey}">Edit</button>
-                <button class="btn btn-danger delete-staff-btn delete-btn" id="${staffObject.firebaseKey}">Delete</button>
+                <button class="btn btn-danger edit-staff-btn edit-btn staff-btn" id="${staffObject.firebaseKey}">Edit</button>
+                <button class="btn btn-danger delete-staff-btn delete-btn staff-btn" id="${staffObject.firebaseKey}">Delete</button>
               </div>
             </div>
             <div class="staff-info">
