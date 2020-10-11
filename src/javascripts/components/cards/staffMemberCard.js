4073f68a03eb2ccda85528f17728b49fbf5481f9
Liz <liz@Liz-s-Mac.local>
2020-10-11 14:05:37 -0500

    build staff card function

---
+++ src/javascripts/components/cards/staffMemberCard.js
@@ -0,0 +1,17 @@
+const buildStaffCard = (staffObject) => {
+  const domString = `
+          <div class="staff-container card-container" id="${staffObject.firebaseKey}">
+            <div class="staff-card card" id="${staffObject.firebaseKey}" style="width: 100%; background-image: url(${staffObject.imageUrl});">
+              <div class="staff-btn-container card-btn-container">
+                <button class="btn btn-danger delete-staff-btn delete-btn" id="${staffObject.firebaseKey}">Delete Board</button>
+              </div>
+            </div>
+            <div class="staff-info">
+              <h5 class="staff-role">${staffObject.role}</h5>
+              <h6 class="staff-name">${staffObject.name}</h6>
+            </div>
+          </div>`;
+  return domString;
+};
+
+export default { buildStaffCard };
