544447b1f45233a40b1dcd3cb65885092e3a4fdc
josephtmartin <goldglovejo13@gmail.com>
2020-10-12 21:50:11 -0500

    displaying the ingredients

--- src/javascripts/components/cards/ingredientsCard.js
+++ src/javascripts/components/cards/ingredientsCard.js
@@ -1,10 +1,12 @@
 const ingredientMaker = (object) => {
   const domString = `<div class="card" style="width: 18rem;" id="${object.uid}>
   <div class="card-body">
-    <h5 class="card-title">${object.ingredient}</h5>
-    <h3 class="card-text">${object.category}</h3>
-    <button id="${object.uid}" class="btn btn-warning update-ingredient-btn" style="color: white"><i class="fas fa-edit"></i></button>
-    <button id="${object.uid}" class="btn btn-danger delete-ingredient"><i id="ingredient-icon" class="fas fa-times"></i></button>
+    <h5 class="card-title">Category: ${object.category}</h5>
+    <h3 class="card-text">Ingredient: ${object.ingredient}</h3>
+    <div class="button-container">
+      <button id="${object.uid}" class="btn btn-warning update-ingredient-btn" style="color: white"><i class="fas fa-edit"></i></button>
+      <button id="${object.uid}" class="btn btn-danger delete-ingredient"><i id="ingredient-icon" class="fas fa-times"></i></button>
+    </div>
   </div>
 </div>`;
   return domString;
--- src/javascripts/components/views/ingredientsView.js
+++ src/javascripts/components/views/ingredientsView.js
@@ -1,7 +1,19 @@
+import data from '../../helpers/data/ingredientsData';
+import card from '../cards/ingredientsCard';
+
 const ingredientsView = () => {
   $('#app').html(`<div id="add-ingredients"></div>
   <button id="add-ingredient-btn" type="button" class="btn btn-info">Add Ingredient</button>
   `);
+  data.getAllIngredients().then((response) => {
+    if (response.length) {
+      response.forEach((item) => {
+        $('#app').append(card.ingredientMaker(item));
+      });
+    } else {
+      $('.alert').html('<h1>No Ingredients</h1>');
+    }
+  });
 };
 
 export default { ingredientsView };
