ee36a785b7d2338310fed45f699ce5aff3d29775
josephtmartin <goldglovejo13@gmail.com>
2020-10-12 19:17:19 -0500

    added the ingredient cards

--- src/javascripts/components/views/ingredientsView.js
+++ src/javascripts/components/views/ingredientsView.js
@@ -1,5 +1,7 @@
 const ingredientsView = () => {
-  $('#app').html('<div id="add-ingredients">Show me the menu!</div>');
+  $('#app').html(`<div id="add-ingredients">Show me the ingredients!</div>
+  <button id="add-ingredient-btn" type="button" class="btn btn-info">Add Ingredient</button>
+  `);
 };
 
 export default { ingredientsView };
