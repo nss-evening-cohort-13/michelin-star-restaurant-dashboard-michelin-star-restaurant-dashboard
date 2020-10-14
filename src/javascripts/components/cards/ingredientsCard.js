ed81e13671865f42c36b6e58c0f036c258e93138
Liz <liz@Liz-s-Mac.local>
2020-10-14 14:36:53 -0500

    missing quotation after id. delete function working

--- src/javascripts/components/cards/ingredientsCard.js
+++ src/javascripts/components/cards/ingredientsCard.js
@@ -1,7 +1,7 @@
 import ingredientData from '../../helpers/data/ingredientsData';
 
 const ingredientMaker = (object) => {
-  const domString = `<div class="ingredient-card card d-flex flex-row m-2 p-1" style="width: 23rem;" id="${object.uid}>
+  const domString = `<div class="ingredient-card card d-flex flex-row m-2 p-1" style="width: 23rem;" id="${object.uid}">
   <div class="card-body d-flex">
     <h3 class="card-text mr-auto p-2">${object.ingredient}</h3>
     <div class="button-container-ingredient ml-2">
