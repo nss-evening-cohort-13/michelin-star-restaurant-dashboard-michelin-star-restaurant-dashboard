f278a54c4be8ff744062d2e8c30bf602566d5e87
Liz <liz@Liz-s-Mac.local>
2020-10-15 21:16:07 -0500

    removed edit guest info

--- src/javascripts/components/forms/editReservation.js
+++ src/javascripts/components/forms/editReservation.js
@@ -11,66 +11,65 @@ const reservationTimes = () => {
 };
 
 // SECOND FORM TO APPEAR
-const editGuestInfo = (reservationObject, reservationFirebaseKey) => {
-  console.warn('guest info object', reservationObject);
-  $('#edit-reservation').html(`<h2>Edit Guest Info</h2>
-        <div id="success-message"></div>
-          <div id="error-message"></div>
-    <form>
-    <div id="input-group">
-        <div class="form-group">
-          <label for="firstName">First Name</label>
-          <input value="${reservationObject.firstName}" type="text" class="form-control" id="firstName" placeholder="First Name">
-        </div>
-          <div class="form-group">
-            <label for="lastName">Last Name</label>
-            <input value="${reservationObject.lastName}" class="form-control" id="lastName" autocomplete="off" placeholder="Last Name">
-          </div>
-          <div class="form-group">
-            <label for="phoneNumber">Phone Number</label>
-            <input value="${reservationObject.phoneNumber}" type="tel" class="form-control" id="phoneNumber" class="timePicker" autocomplete="off" placeholder="ex: 615-123-4567">
-          </div>
-          </div>
-          <button id="updateGuestInfoBtn" type="button" class="btn btn-info"><i class="fas fa-plus-circle"></i> Update Guest Info</button>
-        </form>`);
-  $('#updateGuestInfoBtn').on('click', (e) => {
-    e.preventDefault();
-    //   This is adding the new data to the first data object
-    const data = {
-      firstName: $('#firstName').val() || false,
-      lastName: $('#lastName').val() || false,
-      phoneNumber: $('#phoneNumber').val() || false,
-    };
+// const editGuestInfo = (reservationObject, reservationFirebaseKey) => {
+//   console.warn('guest info object', reservationObject);
+//   $('#edit-reservation').html(`<h2>Edit Guest Info</h2>
+//         <div id="success-message"></div>
+//           <div id="error-message"></div>
+//     <form>
+//     <div id="input-group">
+//         <div class="form-group">
+//           <label for="firstName">First Name</label>
+//           <input value="${reservationObject.firstName}" type="text" class="form-control" id="firstName" placeholder="First Name">
+//         </div>
+//           <div class="form-group">
+//             <label for="lastName">Last Name</label>
+//             <input value="${reservationObject.lastName}" class="form-control" id="lastName" autocomplete="off" placeholder="Last Name">
+//           </div>
+//           <div class="form-group">
+//             <label for="phoneNumber">Phone Number</label>
+//             <input value="${reservationObject.phoneNumber}" type="tel" class="form-control" id="phoneNumber" class="timePicker" autocomplete="off" placeholder="ex: 615-123-4567">
+//           </div>
+//           </div>
+//           <button id="updateGuestInfoBtn" type="button" class="btn btn-info"><i class="fas fa-plus-circle"></i> Update Guest Info</button>
+//         </form>`);
+//   $('#updateGuestInfoBtn').on('click', (e) => {
+//     e.preventDefault();
+//     //   This is adding the new data to the first data object
+//     const data = {
+//       firstName: $('#firstName').val() || false,
+//       lastName: $('#lastName').val() || false,
+//       phoneNumber: $('#phoneNumber').val() || false,
+//     };
 
-    console.warn('guest data', data);
-    if (Object.values(data).includes(false)) {
-      $('#error-message').html(
-        '<div class="alert alert-danger" role="alert">Please complete all fields</div>'
-      );
-    } else {
-      $('#error-message').html('');
-      reservationData.updateReservation(reservationFirebaseKey, data)
-        .then(() => {
-          $('#success-message').html(
-            `<div class="alert alert-success" role="alert">
-          Your reservation was updated!
-          </div>`
-          );
-        }).catch((error) => console.warn(error));
+//     console.warn('guest data', data);
+//     if (Object.values(data).includes(false)) {
+//       $('#error-message').html(
+//         '<div class="alert alert-danger" role="alert">Please complete all fields</div>'
+//       );
+//     } else {
+//       $('#error-message').html('');
+//       reservationData.updateReservation(reservationFirebaseKey, data)
+//         .then(() => {
+//           $('#success-message').html(
+//             `<div class="alert alert-success" role="alert">
+//           Your reservation was updated!
+//           </div>`
+//           );
+//         }).catch((error) => console.warn(error));
 
-      setTimeout(() => {
-        $('#success-message').html('');
-      }, 3000);
-      $('#firstName').val('');
-      $('#lastName').val('');
-      $('#phoneNumber').val('');
-    }
-  });
-};
+//       setTimeout(() => {
+//         $('#success-message').html('');
+//       }, 3000);
+//       $('#firstName').val('');
+//       $('#lastName').val('');
+//       $('#phoneNumber').val('');
+//     }
+//   });
+// };
 
 // First Form to Appear
 const editReservationForm = (reservationObject, reservationFirebaseKey) => {
-  console.warn(reservationFirebaseKey, 'edit res form fbkey');
   $('#edit-reservation').html(`<h2>Edit Reservation</h2>
     <div id="success-message"></div>
     <div>
@@ -99,7 +98,6 @@ const editReservationForm = (reservationObject, reservationFirebaseKey) => {
     <div id="reservation-buttons">
     <button id="seatingBtn" type="button" class="btn btn-primary">View Seating Chart</button>
     <button id="updateReservationBtn" type="button" class="btn btn-primary">Update Reservation</button>
-    <button id="edit-guest-btn" type="button" class="btn btn-info"><i class="fas fa-plus-circle"></i> Edit Guest Info</button>
     </div>
     </div>
     <div id="viewSeats"></div>
@@ -112,10 +110,10 @@ const editReservationForm = (reservationObject, reservationFirebaseKey) => {
   reservationTimes();
 
   // Button click to chow edit guest info form
-  $('body').on('click', '#edit-guest-btn', () => {
-    console.warn('edit guest clicked');
-    editGuestInfo(reservationObject, reservationFirebaseKey);
-  });
+  // $('body').on('click', '#edit-guest-btn', () => {
+  //   console.warn('edit guest clicked');
+  //   editGuestInfo(reservationObject, reservationFirebaseKey);
+  // });
 
   //  Code for seating chart dropdown
   let seatingChartIsNotShown = true;
@@ -148,11 +146,9 @@ const editReservationForm = (reservationObject, reservationFirebaseKey) => {
       );
     } else {
       $('#error-message').html('');
-      editGuestInfo(reservationObject, reservationFirebaseKey);
+      // editGuestInfo(reservationObject, reservationFirebaseKey);
       reservationData.updateReservation(reservationFirebaseKey, data)
         .then(() => {
-          console.warn('update uid', reservationObject);
-          console.warn('update data', data);
           $('#success-message').html(
             `<div class="alert alert-success" role="alert">
           Your reservation was updated!
--- src/javascripts/components/views/editReservationView.js
+++ src/javascripts/components/views/editReservationView.js
@@ -3,10 +3,7 @@ import form from '../forms/editReservation';
 
 const editReservationView = (reservationFirebaseKey) => {
   $('#app').html('<div id="edit-reservation" style="background-color: white;">Hello</div>');
-  console.warn(reservationFirebaseKey, 'res fbkey');
   reservationData.getSingleReservation(reservationFirebaseKey).then((response) => {
-    console.warn(reservationFirebaseKey, 'single res fbkey');
-    console.warn(response, 'sing res response');
     form.editReservationForm(response, response.uid);
   });
 };
