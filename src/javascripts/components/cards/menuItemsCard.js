f774aca8a17587f191c1c951f51b8936a2cd4bb0
Dana Pham <danapham12@gmail.com>
2020-10-21 19:50:05 -0500

    fixed menucards displaying

--- src/javascripts/components/cards/menuItemsCard.js
+++ src/javascripts/components/cards/menuItemsCard.js
@@ -17,35 +17,32 @@ const menuItemCardMaker = (item) => {
 };
 
 const authMenuItemCardMaker = (item) => {
-  let domString = '';
-  menuItemIngredientsData
-    .getMenuItemIngredients(item.id)
-    .then((response) => {
-      console.log(response);
-      domString = `<div class="card-menu m-4" id="${item.id}">
-      <div class="card-body-menu">
-        <div class="menu-item-title">
-          <h4 class="card-text-menu text-uppercase">${item.name}</h4>
-          <p id="menuItemPrice">${item.price}</p>
-        </div>
-        <div id="menuItemInfo" class="menuItemInfo">
-          <p id="listOfIngredients" class="text-lowercase">${response.join(' | ')}</p>
-          <div class="button-container-menu">
-            <button id="${item.id}" class="btn btn-outline update-menu-item-btn icon-btn" style="color: white"><i class="fas fa-edit"></i></button>
-            <button id="${item.id}" class="btn btn-outline delete-menu-item icon-btn"><i id="ingredient-icon" class="fas fa-times"></i></button>
+  const domString = `<div class="card-menu m-4" id="${item.id}">
+        <div class="card-body-menu">
+          <div class="menu-item-title">
+            <h4 class="card-text-menu text-uppercase">${item.name}</h4>
+            <p id="menuItemPrice">${item.price}</p>
+          </div>
+          <div id="menuItemInfo" class="menuItemInfo">
+          <p id="listOfIngredients-${item.id}" class="text-lowercase"></p>
+            <div class="button-container-menu">
+              <button id="${item.id}" class="btn btn-outline update-menu-item-btn icon-btn" style="color: white"><i class="fas fa-edit"></i></button>
+              <button id="${item.id}" class="btn btn-outline delete-menu-item icon-btn"><i id="ingredient-icon" class="fas fa-times"></i></button>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-`;
-    });
-
+      </div>`;
   $('body').on('click', '.delete-menu-item', (e) => {
     e.stopImmediatePropagation();
     const firebaseKey = e.currentTarget.id;
     $(`.card-menu#${firebaseKey}`).remove();
     menuData.deleteMenuItem(firebaseKey);
   });
+  menuItemIngredientsData
+    .getMenuItemIngredients(item.id)
+    .then((response) => {
+      $(`#listOfIngredients-${item.id}`).html(response.join(' | '));
+    });
   return domString;
 };
 
--- src/javascripts/helpers/data/menuItemIngredientsData.js
+++ src/javascripts/helpers/data/menuItemIngredientsData.js
@@ -1,26 +1,49 @@
 import axios from 'axios';
 import apiKeys from '../apiKeys.json';
-// import ingredientsData from './ingredientsData';
+import ingredientsData from './ingredientsData';
 
 const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
 const addMenuItemIngredients = (data) => axios.post(`${baseUrl}/menuItems_ingredients.json`, data)
   .catch((error) => console.warn(error));
 
+// const getMenuItemIngredients = (menuItemId) => new Promise((resolve, reject) => {
+//   const menuItemIngredientNames = [];
+//   axios
+//     .get(`${baseUrl}/menuItems_ingredients.json?orderBy="menuItemId"&equalTo="${menuItemId}"`)
+//     .then((res) => {
+//       Object.keys(res.data).forEach((ingredientId) => {
+//         axios.get(`${baseUrl}/ingredients/${res.data[ingredientId].ingredientId}.json`).then((response) => {
+//           const singleIngredient = response.data;
+//           menuItemIngredientNames.push(singleIngredient.ingredient);
+//         });
+//       });
+//       if (menuItemIngredientNames.length === Object.keys(res.data).length) {
+//         resolve(menuItemIngredientNames);
+//       }
+//     }).catch((error) => reject(error));
+// });
+const objToArray = (objOfObjs) => {
+  const array = [];
+  Object.keys(objOfObjs).forEach((key) => {
+    array.push(objOfObjs[key]);
+  });
+  return array;
+};
+
 const getMenuItemIngredients = (menuItemId) => new Promise((resolve, reject) => {
-  const menuItemIngredientNames = [];
-  axios
-    .get(`${baseUrl}/menuItems_ingredients.json?orderBy="menuItemId"&equalTo="${menuItemId}"`)
-    .then((res) => {
-      Object.keys(res.data).forEach((ingredientId) => {
-        axios.get(`${baseUrl}/ingredients/${res.data[ingredientId].ingredientId}.json`).then((response) => {
-          const singleIngredient = response.data;
-          menuItemIngredientNames.push(singleIngredient.ingredient);
+  axios.get(`${baseUrl}/menuItems_ingredients.json?orderBy="menuItemId"&equalTo="${menuItemId}"`)
+    .then((menuRes) => {
+      const menuArray = objToArray(menuRes.data);
+      ingredientsData.getAllIngredients().then((ingredientsRes) => {
+        const ingredientNames = [];
+        menuArray.forEach((menuIngredient) => {
+          const menuObject = ingredientsRes.find((ingredient) => ingredient.uid === menuIngredient.ingredientId);
+          const menuIngredientUse = menuObject.ingredient;
+          ingredientNames.push(menuIngredientUse);
+          resolve(ingredientNames);
         });
       });
-      if (menuItemIngredientNames.length === Object.keys(res.data).length) {
-        resolve(menuItemIngredientNames);
-      }
     }).catch((error) => reject(error));
 });
 
