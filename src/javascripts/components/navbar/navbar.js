6911d19a7e7fa897c956ba69f28dafe4fa8cbc3d
Dana Pham <danapham12@gmail.com>
2020-10-31 10:03:12 -0500

    made reports only viewable in authed view

--- src/javascripts/components/navbar/navbar.js
+++ src/javascripts/components/navbar/navbar.js
@@ -1,4 +1,40 @@
-const navbar = () => {
+// import firebase from 'firebase/app';
+// import 'firebase/auth';
+
+const authNavbar = () => {
+  $('#nav').html(`
+    <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#" style="color: white;">Le Baguette</a>
+      <button id="navBarToggle" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" 
+    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+      <li class="nav-item mx-3"  id="menuLink">
+        <a class="nav-link" id="menuLink" href="#" style="color: white;">Menu</a>
+      </li>
+      <li class="nav-item mx-3" id="reservationLink">
+        <a class="nav-link" id="reservationLink" href="#" style="color: white;">Reservation</a>
+      </li>
+      <li class="nav-item mx-3" id="staffLink">
+        <a class="nav-link" id="staffLink" href="#" style="color: white;">Staff</a>
+      </li>
+      <li class="nav-item mx-3" id="reportsLink">
+      <a class="nav-link" id="reportsLink" href="#" style="color: white;">Reports</a>
+    </li>
+    </ul>
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item" id="loginLogout">
+          </li>
+        </ul>
+      </div>
+    </nav>
+    `);
+};
+
+const unauthedNavbar = () => {
   $('#nav').html(`
   <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#" style="color: white;">Le Baguette</a>
@@ -18,11 +54,7 @@ const navbar = () => {
     <li class="nav-item mx-3" id="staffLink">
       <a class="nav-link" id="staffLink" href="#" style="color: white;">Staff</a>
     </li>
-    <li class="nav-item mx-3" id="reportsLink">
-    <a class="nav-link" id="reportsLink" href="#" style="color: white;">Reports</a>
-  </li>
   </ul>
-
       <ul class="navbar-nav ml-auto">
         <li class="nav-item" id="loginLogout">
         </li>
@@ -32,4 +64,4 @@ const navbar = () => {
   `);
 };
 
-export default { navbar };
+export default { authNavbar, unauthedNavbar };
--- src/javascripts/components/views/reportsView.js
+++ src/javascripts/components/views/reportsView.js
@@ -1,16 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import popularItemsCard from '../cards/popularItemsCard';
 
 const reportsView = () => {
-  $('#app').append(`<div id="popularMenuItemsDiv">
-  <h3>Most Popular Menu Items</h3>
-  <ol id="popularItemsList"></ol>
-  </div>
-  <div id="leastPopularDiv">
-  <h3>Least Popular Menu Items</h3>
-  <ol id="leastPopularList"></ol>
-  </div>`);
-  popularItemsCard.popularItems();
-  popularItemsCard.leastPopularItems();
+  const user = firebase.auth().currentUser;
+
+  if (user) {
+    $('#app').append(`<div id="popularMenuItemsDiv">
+    <h3>Most Popular Menu Items</h3>
+    <ol id="popularItemsList"></ol>
+    </div>
+    <div id="leastPopularDiv">
+    <h3>Least Popular Menu Items</h3>
+    <ol id="leastPopularList"></ol>
+    </div>`);
+    popularItemsCard.popularItems();
+    popularItemsCard.leastPopularItems();
+  }
 };
 
 export default { reportsView };
